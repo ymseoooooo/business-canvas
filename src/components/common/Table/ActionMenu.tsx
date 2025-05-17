863f7354f913558a7a3333ceb57d507c9f3790b2
ymseoooooo <ymseoooooo@gmail.com>
2025-05-17 15:25:34 +0900

    ActionMenu component add

---
+++ src/components/common/Table/ActionMenu.tsx
@@ -0,0 +1,12 @@
+import { Button, Dropdown, DropdownProps } from 'antd';
+import { MoreOutlined } from '@ant-design/icons';
+
+type ActionMenuProps = Omit<DropdownProps, 'children'>;
+
+export function ActionMenu(props: ActionMenuProps) {
+  return (
+    <Dropdown {...props}>
+      <Button type="text" icon={<MoreOutlined />} size="small" />
+    </Dropdown>
+  );
+}
